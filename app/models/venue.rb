cf6754f641fed9dc24424851d62e6657673a1ddd
keisuke nagamatsu <ngmt.k0520@gmail.com>
2026-04-24 12:55:13 +0900

    Venue新規登録、実装

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -2,4 +2,7 @@ class Venue < ApplicationRecord
   has_many :events
 
   validates :name, presence: true
+  validates :area, presence: true
+
+  AREAS = ["福岡県", "佐賀県", "長崎県", "熊本県", "大分県", "宮崎県", "鹿児島県",]
 end
