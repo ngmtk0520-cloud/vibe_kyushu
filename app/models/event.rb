f376dbbcaa7392a9d73f647998796a32c4e00077
keisuke nagamatsu <ngmt.k0520@gmail.com>
2026-04-24 13:29:26 +0900

    ジャンル選択　実装

--- app/models/event.rb
+++ app/models/event.rb
@@ -7,4 +7,6 @@ class Event < ApplicationRecord
   validates :description, presence: true
   validates :image, presence: true
   validates :fee, presence: true
+
+  GENRES = ["Reggae", "Hip-Hop", "Trance", "Techno", "House", "EDM", "J-pop", "Rock"].freeze
 end
