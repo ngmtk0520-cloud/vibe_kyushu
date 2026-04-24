7e5d683d1f9dcffc10acdb3f276db7cd36d1a3e3
keisuke nagamatsu <ngmt.k0520@gmail.com>
2026-04-24 12:24:11 +0900

    バリデーション追加

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,5 +2,9 @@ class Event < ApplicationRecord
   belongs_to :venue
   belongs_to :user
 
-  validates :name, presence: true
+  validates :title, presence: true
+  validates :date, presence: true
+  validates :description, presence: true
+  validates :image, presence: true
+  validates :fee, presence: true
 end
